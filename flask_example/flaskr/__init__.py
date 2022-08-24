8ccfb99305960eab9a64f55fbeae2c7737148777
ginohmk <michotall95@gmail.com>
2022-08-24 07:01:42 +0100

    Cors

--- flask_example/flaskr/__init__.py
+++ flask_example/flaskr/__init__.py
@@ -1,7 +1,24 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 def create_app(test_configure= None):
     app = Flask(__name__)
+
+    #CORS APP
+    cors = CORS(app, resources = {r"/api/*":{"origins":"*"}})
+
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+                response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization,true')
+                response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+                return response
+
+    @app.route('/mesages')
+    @cross_origin()
+    def get_messages():
+                return 'GETTING MESSAGES'
+
 
 
     # define endpoint
