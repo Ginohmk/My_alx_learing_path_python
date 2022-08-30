33d5c6145e19767fc57b003e7a819b83c60b886b
ginohmk <michotall95@gmail.com>
2022-08-30 19:00:51 +0100

    Add all plant Complete working code

--- flask_example/models.py
+++ flask_example/models.py
@@ -5,7 +5,7 @@ import json
 from sqlalchemy.sql.schema import PrimaryKeyConstraint
 
 database_name = "plants"
-database_path ="postgres://{}:{}@{}/{}".format('student', 'student','localhost:5432', database_name)
+database_path ="postgresql://{}:{}@{}/{}".format('mike-savy', 'dreamlife!','localhost:5432', database_name)
 
 db = SQLAlchemy()
 
@@ -13,6 +13,7 @@ db = SQLAlchemy()
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    PYTHONUNBUFFERED= ""
     db.app = app
     db.init_app(app)
     db.create_all()
